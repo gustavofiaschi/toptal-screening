dba493065636ac33e569807acdb4403d02c947dd
Clevertech <Clevertech@DELL-G15>
2024-01-29 16:05:41 -0300

    Add project files.

---
+++ toptal-screening/OrderModel.cs
@@ -0,0 +1,86 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace toptal_screening
+{
+    public partial class Order
+    {
+        [JsonProperty("order_id")]
+        public long OrderId { get; set; }
+
+        [JsonProperty("order_number")]
+        public long OrderNumber { get; set; }
+
+        [JsonProperty("customer_id")]
+        public long CustomerId { get; set; }
+
+        [JsonProperty("customer_name")]
+        public string CustomerName { get; set; }
+
+        [JsonProperty("seller_id")]
+        public long SellerId { get; set; }
+
+        [JsonProperty("seller_name")]
+        public string SellerName { get; set; }
+
+        [JsonProperty("order_items")]
+        public OrderItem[] OrderItems { get; set; }
+
+        [JsonProperty("shipping_address")]
+        public ShippingAddress ShippingAddress { get; set; }
+
+        [JsonProperty("loyalty_points")]
+        public long LoyaltyPoints { get; set; }
+
+        [JsonProperty("order_status", NullValueHandling = NullValueHandling.Ignore)]
+        public string OrderStatus { get; set; }
+
+        [JsonProperty("created_at")]
+        public DateTimeOffset CreatedAt { get; set; }
+
+        [JsonProperty("updated_at")]
+        public DateTimeOffset UpdatedAt { get; set; }
+    }
+
+    public partial class OrderItem
+    {
+        [JsonProperty("product_id")]
+        public long ProductId { get; set; }
+
+        [JsonProperty("product_name")]
+        public string ProductName { get; set; }
+
+        [JsonProperty("product_category")]
+        public ProductCategory ProductCategory { get; set; }
+
+        [JsonProperty("quantity")]
+        public long Quantity { get; set; }
+
+        [JsonProperty("unit_price")]
+        public double UnitPrice { get; set; }
+    }
+
+    public partial class ShippingAddress
+    {
+        [JsonProperty("line")]
+        public string Line { get; set; }
+
+        [JsonProperty("city")]
+        public string City { get; set; }
+
+        [JsonProperty("state")]
+        public string State { get; set; }
+
+        [JsonProperty("postal_code")]
+        public string PostalCode { get; set; }
+
+        [JsonProperty("country")]
+        public string Country { get; set; }
+    }
+
+    public enum ProductCategory { Electronics, Grocery, HomeAppliances, PowerTools };
+}
---
+++ toptal-screening/Program.cs
@@ -0,0 +1,51 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using toptal_screening;
+
+var client = new HttpClient();
+
+//Items 1 and 2 - Read and convert Json Data to Model
+var jsonString = await client.GetStringAsync("https://topt.al/kjcmWA");
+var jsonContent = Order.FromJson(jsonString);
+
+//Item 3 - Show Order id and number
+Console.WriteLine($"Item 3:");
+foreach (var order in jsonContent)
+{    
+    Console.WriteLine($"{order.OrderId}, {order.OrderNumber}");
+}
+
+//Item 4 - Show order status and total of same status 
+Console.WriteLine($"Item 4:");
+foreach (var order in jsonContent.OrderBy(x => x.CustomerName))
+{
+    var sumStatus = jsonContent.Count(x => x.OrderStatus == order.OrderStatus);    
+    Console.WriteLine($"{order.OrderId}, {order.CustomerName}, {order.OrderStatus}, Qtd orders status: {sumStatus}");
+}
+
+//Item 5 - Show amount of items with Grocery produts
+Console.WriteLine($"Item 5:");
+foreach (var order in jsonContent.OrderBy(x => x.CustomerName))
+{
+    if (order.OrderItems.Any(o => o.ProductCategory == ProductCategory.Grocery))
+    {
+        var sumOrderItems = order.OrderItems.Sum(o => o.UnitPrice);
+        if (sumOrderItems < 100)
+        {
+            Console.WriteLine($"{order.OrderId}, Total price: {sumOrderItems}");
+        }
+    }
+}
+
+//Item 6 - Show avg of prices in a single order
+Console.WriteLine("Item 6:");
+foreach (var order in jsonContent.OrderBy(x => x.CustomerName))
+{
+    var avgPrice = order.OrderItems.Average(o => o.UnitPrice);
+    Console.WriteLine($"{order.OrderId}, avg: {avgPrice}");
+
+}
+
+Console.ReadLine();
+
+
